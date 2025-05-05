4aef5675c01b3c0faf9faa3e68905d0b451a13e9
maeka <maek.alekdawi@gmail.com>
2025-05-05 10:08:39 +0200

    Changed login and test works

--- src/main/java/se/branko/atm/service/AuthService.java
+++ src/main/java/se/branko/atm/service/AuthService.java
@@ -2,6 +2,6 @@ package se.branko.atm.service;
 
 public class AuthService {
     public boolean login(String cardNumber, String pin) {
-        return false;
+        return "12345678".equals(cardNumber)&&"1234".equals(pin);
     }
 }
